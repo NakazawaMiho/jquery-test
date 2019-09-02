ee0867b9db634b3d0f1c0a2023f7ff0fdd7e4c13
Miho Nakazawa <llzo.31h0@gmail.com>
2019-09-03 00:01:25 +0900

    initial commit

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 $(function () {
-  var $textField = $('#title');
-  console.log($textField);
+  // var $textField = $('#title');
+  // console.log($textField);
   // 処理の説明を入れてください
   //　例：ボタンが押されると、sub-titleがremoveされる。
   $("#removeButton").on("click", function () {
     $('.sub-title').remove();
-  });
 });
+
+  //ボタンが押されると、grandfatherがfadeOutされる。
+  $("#fadeOutButton").on("click",function(){
+    $('#grandfather').fadeOut();
+  });
+
+  $("#hideButton").on("click",function(){
+    $('#mother').hide();
+  });
+
+  $("#showButton").on("click",function(){
+    $('#mother').show();
+  });
+// $(function (){
+  
+//   });
+// });
+})
